b4c2c584854d7ee809e6cea5345e69c5ecee927c
Victor Alcaraz <vctralcaraz@gmail.com>
2023-12-02 11:14:54 -0800

    phase 2 complete

--- phase-2.js
+++ phase-2.js
@@ -1,24 +1,79 @@
 function stretch(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  const ms = 1000;
+
+  let promise = new Promise((resolve, reject) => {
+    if(timeLeft < ms) {
+      reject('Error: you dont have enough time to stretch')
+    } else {
+
+      setTimeout(() => {
+        resolve(timeLeft -= ms);
+        console.log('done stretching');
+      }, ms);
+
+    }
+
+  });
+
+  return promise;
 }
 
 
 function runOnTreadmill(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  const ms = 500;
+
+  let promise = new Promise((resolve, reject) => {
+    if(timeLeft < ms) {
+      reject('Error: you dont have enough time to run on treadmill');
+    } else {
+
+      setTimeout(() => {
+        resolve(timeLeft -= ms);
+        console.log('done running on treadmill');
+      }, ms);
+
+    }
+  });
+
+  return promise;
 }
 
 
 function liftWeights(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  const ms = 2000;
+
+  let promise = new Promise((resolve, reject) => {
+    if(timeLeft < ms) {
+      reject('Error: you dont have enough time to left weights');
+    } else {
+
+      setTimeout(() => {
+        resolve(timeLeft - ms);
+        console.log('done lifting weights');
+      }, ms);
+
+    }
+  });
+
+  return promise;
 }
 
 
 function workout(totalTime) {
   // refactor your code from phase 1
   // Your code here
+
+  stretch(totalTime)
+    .then(timeLeft => runOnTreadmill(timeLeft))
+    .then(timeLeft => liftWeights(timeLeft))
+    .then(timeLeft => console.log(`done working out with ${timeLeft / 1000 } seconds left`))
+    .catch(error => console.log(error));
 }
 
 /* ============================ TEST YOUR CODE ============================
@@ -46,7 +101,7 @@ Comment in each invocation of your workout function below and run the file
     // Error:  you dont have enough time to lift weights
 
 
-// workout(4000);
+workout(4000);
   // should print out the following:
   //   done stretching
   //   done running on treadmill
